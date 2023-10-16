9c9c12b904dcdd121826822bffec10c12de11bd8
aleleh <alexander.lehnbom@gmail.com>
2023-10-16 15:08:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+Hangman Game
+Welcome to the Hangman game! This classic word-guessing game challenges players to guess the computer-selected word one letter at a time. But beware, too many incorrect guesses and you might just hang the hangman!
+
+How to Play
+Start the Game:
+
+Run the script to initiate the game.
+Guess a Letter:
+
+You'll be presented with a series of underscores representing the letters in the word.
+Your task is to guess the letters in the word.
+Enter a single letter to make a guess.
+Feedback:
+
+After each guess, the game will provide feedback.
+If your guess is correct, the relevant underscore(s) will be replaced by the guessed letter.
+If your guess is incorrect, a part of the hangman figure will be drawn.
+Winning & Losing:
+
+If you guess the word before the hangman figure is fully drawn, you win!
+If the hangman figure is completed before you guess the word, you lose.
+Upon winning or losing, the correct word will be revealed.
+
+Rules
+Only single-letter alphabetical guesses are allowed.
+The game is case-insensitive; 'A' and 'a' are treated the same.
+Repeated guesses of the same letter will not be penalized, but try to avoid them for the best experience.
+
+Prerequisites
+This game requires Python to be installed on your machine.
+No additional libraries or modules are required beyond the standard Python libraries.
+
+Known Issues
+None at the moment. Please report if you find any!
+Feedback and Contribution
+Feel free to provide feedback or make contributions to this project. If you encounter any issues or have suggestions, please open an issue or make a pull request.
---
+++ main.py
@@ -0,0 +1,120 @@
+import random
+
+def choose_word():
+    word_list = ['python', 'javascript', 'java', 'swift', 'golang', 'ruby']
+    return random.choice(word_list)
+
+def display_hangman(tries):
+    stages = [  # hangman progression
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     /
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |      O
+                   |
+                   |
+                   |
+                   -
+                """,
+                """
+                   --------
+                   |      |
+                   |
+                   |
+                   |
+                   |
+                   -
+                """
+    ]
+    return stages[6 - tries]
+
+def play_game(word):
+    word_completion = '_' * len(word)
+    guessed = False
+    guessed_letters = []
+    tries = 6
+
+    print("Let's play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter: ").lower()
+        
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        else:
+            print("Not a valid guess.")
+
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+
+    if guessed:
+        print("Congrats! You guessed the word!")
+    else:
+        print("Sorry, you ran out of tries. The word was " + word)
+
+if __name__ == "__main__":
+    word = choose_word()
+    play_game(word)
